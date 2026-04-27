af4a35d8006e3f4228581fa44120bc9673c71898
Aaka2203 <aakarshsaxena0001@gmail.com>
2026-04-27 14:47:14 +0530

    Aakarsh:Completed this ReverseLInkedList question.

---
+++ ReverseLinkedList_13.java
@@ -0,0 +1,34 @@
+public class ReverseLinkedList_13 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        head = reverseList(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
